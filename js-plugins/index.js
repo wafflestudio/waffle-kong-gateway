3de92e0c89e9b766cd6d29a4832b99efcc2a1d5e
davin111 <bdv111@snu.ac.kr>
2022-10-24 22:50:04 +0900

    rm dotenv and block waffle-user-id from external requests

--- js-plugins/index.js
+++ js-plugins/index.js
@@ -1,10 +1,8 @@
-'use strict';
+"use strict";
 
-require('dotenv').config();
+const jwt = require("jsonwebtoken");
 
-const jwt = require('jsonwebtoken');
-
-const publicKey = '-----BEGIN PUBLIC KEY-----\n' + process.env.WAFFLE_JWT_PUBLIC_KEY + '\n-----END PUBLIC KEY-----';
+const publicKey = "-----BEGIN PUBLIC KEY-----\n" + process.env.WAFFLE_JWT_PUBLIC_KEY + "\n-----END PUBLIC KEY-----";
 const issuer = process.env.WAFFLE_JWT_ISSUER;
 const authPrefix = "Bearer "
 
@@ -14,6 +12,11 @@ class KongPlugin {
     }
 
     async access(kong) {
+        const waffleUserId = await kong.request.get_header("waffle-user-id");
+        if (waffleUserId !== null || waffleUserId !== undefined) {
+            return kong.response.exit(403);
+        }
+
         const authorization = await kong.request.getHeader("authorization")
         if (authorization === undefined || !authorization.startsWith(authPrefix)) {
             return;
@@ -22,23 +25,23 @@ class KongPlugin {
         const accessToken = authorization.substring(authPrefix.length);
         let payload;
         try {
-            payload = jwt.verify(accessToken, publicKey, { algorithms: ['RS512'], issuer: issuer });
+            payload = jwt.verify(accessToken, publicKey, { algorithms: ["RS512"], issuer: issuer });
         } catch (err) {
             if (err instanceof jwt.TokenExpiredError || err instanceof jwt.JsonWebTokenError) {
                 return kong.response.exit(403);
             }
             throw err;
         }
-        await kong.service.request.setHeader('waffle-user-id', payload.sub);
+        await kong.service.request.setHeader("waffle-user-id", payload.sub);
     }
 }
 
 module.exports = {
     Plugin: KongPlugin,
-    Name: 'waffle-jwt-authorizer',
+    Name: "waffle-jwt-authorizer",
     Schema: [
         { message: { type: "string" } },
     ],
-    Version: '0.1.0',
+    Version: "0.1.0",
     Priority: 0,
 }
