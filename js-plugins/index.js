84ef8b0e2f409fc43ebd363c808558391b7687c2
davin111 <bdv111@snu.ac.kr>
2022-10-24 23:16:50 +0900

    lint

--- js-plugins/index.js
+++ js-plugins/index.js
@@ -1,50 +1,50 @@
-"use strict";
+'use strict';
 
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 
-const publicKey = "-----BEGIN PUBLIC KEY-----\n" + process.env.WAFFLE_JWT_PUBLIC_KEY + "\n-----END PUBLIC KEY-----";
+const publicKey = '-----BEGIN PUBLIC KEY-----\n' + process.env.WAFFLE_JWT_PUBLIC_KEY + '\n-----END PUBLIC KEY-----';
 const issuer = process.env.WAFFLE_JWT_ISSUER;
-const authPrefix = "Bearer "
+const authPrefix = 'Bearer ';
 
 class KongPlugin {
-    constructor(config) {
-        this.config = config;
+  constructor(config) {
+    this.config = config;
+  }
+
+  async access(kong) {
+    const waffleUserId = await kong.request.getHeader('waffle-user-id');
+    // block requests with manual 'waffle-user-id' header
+    if (waffleUserId != undefined) {
+      return kong.response.exit(403);
     }
 
-    async access(kong) {
-        const waffleUserId = await kong.request.getHeader("waffle-user-id");
-        // block requests with manual 'waffle-user-id' header
-        if (waffleUserId != undefined) {
-            return kong.response.exit(403);
-        }
-
-        const authorization = await kong.request.getHeader("authorization")
-        // allow requests without 'authorization' header because some apis may be public
-        // each service can choose to allow or block requests without 'waffle-user-id' header per api
-        if (authorization === undefined || !authorization.startsWith(authPrefix)) {
-            return;
-        }
-
-        const accessToken = authorization.substring(authPrefix.length);
-        let payload;
-        try {
-            payload = jwt.verify(accessToken, publicKey, { algorithms: ["RS512"], issuer: issuer });
-        } catch (err) {
-            if (err instanceof jwt.TokenExpiredError || err instanceof jwt.JsonWebTokenError) {
-                return kong.response.exit(403);
-            }
-            throw err;
-        }
-        await kong.service.request.setHeader("waffle-user-id", payload.sub);
+    const authorization = await kong.request.getHeader('authorization')
+    // allow requests without 'authorization' header because some apis may be public
+    // each service can choose to allow or block requests without 'waffle-user-id' header per api
+    if (authorization === undefined || !authorization.startsWith(authPrefix)) {
+      return;
     }
+
+    const accessToken = authorization.substring(authPrefix.length);
+    let payload;
+    try {
+      payload = jwt.verify(accessToken, publicKey, { algorithms: ['RS512'], issuer: issuer });
+    } catch (err) {
+      if (err instanceof jwt.TokenExpiredError || err instanceof jwt.JsonWebTokenError) {
+        return kong.response.exit(403);
+      }
+      throw err;
+    }
+    await kong.service.request.setHeader('waffle-user-id', payload.sub);
+  }
 }
 
 module.exports = {
-    Plugin: KongPlugin,
-    Name: "waffle-jwt-authorizer",
-    Schema: [
-        { message: { type: "string" } },
-    ],
-    Version: "0.1.0",
-    Priority: 0,
+  Plugin: KongPlugin,
+  Name: 'waffle-jwt-authorizer',
+  Schema: [
+    { message: { type: 'string' } },
+  ],
+  Version: '0.1.0',
+  Priority: 0,
 }
